6299ba81357acadcca080a20e82a6d484696441d
Daniel Kim <danielkim@Daniels-MBP.lan>
2023-11-02 15:59:59 -0700

    added add card modal

--- src/client/components/InterviewItem.js
+++ src/client/components/InterviewItem.js
@@ -8,6 +8,10 @@ import { useNavigate } from 'react-router-dom';
 const InterviewItem = ({ date, position, company, index }) => {
   const navigate = useNavigate();
   const [visible, setVisible] = useState(false);
+  
+  const handleClick = ()=>{
+    navigate('/Deck')
+  }
 
   useEffect(() => {
     // Delay the appearance of each item by multiplying the index
@@ -20,7 +24,7 @@ const InterviewItem = ({ date, position, company, index }) => {
   }, [index]);
 
   return (
-    <div className={`itemContainer hvr-float rounded-md bg-white bg-clip-border text-gray-700 shadow-md ${visible ? 'fade-in' : ''}`}>
+    <div onClick={handleClick} className={`itemContainer hvr-float rounded-md bg-white bg-clip-border text-gray-700 shadow-md ${visible ? 'fade-in' : ''}`}>
       <div className="interviewDate">{date}</div>
       <div className="companyNameTitle">{company}</div>
       <div className="positionTitle">{position}</div>
